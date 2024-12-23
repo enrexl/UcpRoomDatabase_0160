e07bb2e649a572aeb478a46b0498afc74664035d
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 20:45:56 +0700

    Menambah initializer view model halaman lain pada PenyediaViewModel.kt

--- app/src/main/java/com/example/ersa/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ersa/ui/viewmodel/PenyediaViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.ersa.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ersa.KrsApp
-import com.example.ersa.ui.view.dosen.HomeDosenView
-import com.example.ersa.ui.view.dosen.InsertDosenView
+import com.example.ersa.ui.view.matakuliah.UpdateMKView
+import com.example.ersa.ui.viewmodel.dosen.HomeDosenViewModel
+import com.example.ersa.ui.viewmodel.dosen.InsertDosenViewModel
+import com.example.ersa.ui.viewmodel.matakuliah.DetailMKViewModel
+import com.example.ersa.ui.viewmodel.matakuliah.HomeMKViewModel
+import com.example.ersa.ui.viewmodel.matakuliah.MataKuliahViewModel
+import com.example.ersa.ui.viewmodel.matakuliah.UpdateMKViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -19,7 +25,37 @@ object PenyediaViewModel {
         initializer {
             InsertDosenViewModel(
                 KrsApp().containerApp.repositoryDosen
-            ) }
+            )
+        }
+
+        initializer {
+            HomeMKViewModel(
+                KrsApp().containerApp.repositoryMataKuliah
+            )
+        }
+
+        initializer {
+            MataKuliahViewModel(
+                KrsApp().containerApp.repositoryMataKuliah,
+                //KrsApp().containerApp.repositoryDosen
+            )
+        }
+
+        initializer {
+            UpdateMKViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMataKuliah
+            )
+        }
+
+        initializer {
+            DetailMKViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMataKuliah
+            )
+        }
+
+
     }
 }
 
