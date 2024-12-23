f6c5f907b42e7c4ae01e6735e2980d3d93d64714
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 11:08:06 +0700

    membuat fungsi form dosen pada InsertDosenView.kt

--- app/src/main/java/com/example/ersa/ui/view/dosen/InsertDosenView.kt
+++ app/src/main/java/com/example/ersa/ui/view/dosen/InsertDosenView.kt
@@ -1,2 +1,135 @@
 package com.example.ersa.ui.view.dosen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ersa.ui.navigation.AlamatNavigasi
+import com.example.ersa.ui.viewmodel.DosenEvent
+import com.example.ersa.ui.viewmodel.FormErrorState
+import com.example.ersa.ui.viewmodel.InsertDosenViewModel
+import com.example.ersa.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+
+object DestinasiInsertDosen: AlamatNavigasi{
+    override val route: String = "insert_dosen"
+}
+
+@Composable
+fun InsertDosenView(
+    viewModel: InsertDosenViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack:() -> Unit,
+    modifier: Modifier = Modifier
+){
+    val uiState = viewModel.uiState //ambil ui state dari viewmodel
+    val snackbarHostState = remember { SnackbarHostState() } //Snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    //Observasi perubahan snackBarMessage
+    LaunchedEffect(uiState.snackBarMessage)
+    {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch{
+                snackbarHostState.showSnackbar(message) // Tampilkan snackBar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent = DosenEvent(),
+    onValueChange: (DosenEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+
+){
+    val jenisKelamin = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = modifier.fillMaxWidth())
+    {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nidn,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nidn = it))
+
+            },
+            label = { Text("NIDN") },
+            isError = errorState.nidn != null,
+            placeholder = { Text("Masukkan NIDN") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(
+            text = errorState.nidn ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = dosenEvent.nama,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama") },
+
+            )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Jenis Kelamin")
+        Row(modifier = Modifier.fillMaxWidth())
+        {
+            jenisKelamin.forEach { jk ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = dosenEvent.jenisKelamin == jk,
+                        onClick = {
+                            onValueChange(dosenEvent.copy(jenisKelamin = jk))
+                        }
+                    )
+                    Text(
+                        text = jk,
+                    )
+                }
+            }
+        }
+        Text(
+            text = errorState.jenisKelamin ?: "",
+            color = Color.Red
+        )
+    }
+}
