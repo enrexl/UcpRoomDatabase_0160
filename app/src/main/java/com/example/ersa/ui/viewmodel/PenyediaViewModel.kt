08dc24c847ca58fbc70142f3f859a666df07e5d2
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 10:24:00 +0700

    Mengisi PenyediaViewModel.kt

--- app/src/main/java/com/example/ersa/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ersa/ui/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.ersa.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ersa.KrsApp
+import com.example.ersa.ui.view.dosen.HomeDosenView
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeDosenViewModel(
+                KrsApp().containerApp.repositoryDosen
+            )
+        }
+    }
+}
+
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
