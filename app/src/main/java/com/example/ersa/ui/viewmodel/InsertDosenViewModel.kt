5e4c33996f5de12998593c9e89087578aebc2ee3
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 10:40:31 +0700

    Membuat DosenEvent dan error state pada InsertDosenViewModel.kt

--- app/src/main/java/com/example/ersa/ui/viewmodel/InsertDosenViewModel.kt
+++ app/src/main/java/com/example/ersa/ui/viewmodel/InsertDosenViewModel.kt
@@ -1,2 +1,51 @@
 package com.example.ersa.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ersa.repository.RepositoryDosen
+import com.example.ersa.data.entity.Dosen
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class InsertDosenViewModel (private val repositoryDosen: RepositoryDosen) : ViewModel(){
+
+    var uiState by mutableStateOf(DosenUIState())
+
+    fun updateState(dosenEvent: DosenEvent){
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent
+        )
+    }
+
+    private fun validateFields()
+}
+
+data class DosenUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null
+)
+
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null
+){
+    fun isValid(): Boolean{
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
+
+//data class Variable yang menyimpan data input form
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
+)
+
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin
+)
