ef935e5311f0551e49299e83f182ca5fa8ce5ac1
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 20:40:20 +0700

    Menambah fungsi HomMKiew pada HomeMKView.kt

--- app/src/main/java/com/example/ersa/ui/view/matakuliah/HomeMKView.kt
+++ app/src/main/java/com/example/ersa/ui/view/matakuliah/HomeMKView.kt
@@ -10,53 +10,120 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 import com.example.ersa.data.entity.MataKuliah
-import com.example.ersa.ui.view.dosen.ListDosen
-import com.example.ersa.ui.viewmodel.HomeUiState
+import com.example.ersa.ui.customwidget.CustomTopAppBar
+import com.example.ersa.ui.viewmodel.matakuliah.HomeMKViewModel
+import com.example.ersa.ui.viewmodel.matakuliah.HomeUiStateMK
+import com.example.ersa.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
+
+@Preview (showBackground = true)
+@Composable
+fun HomeMKView(
+    viewModel: HomeMKViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs:() -> Unit = {},
+    onDetailClick: (String) -> Unit ={},
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = true,
+                onBack = {},
+                modifier = modifier
+            )
+
+        },
+        floatingActionButton = {                // tombol add di kanan bawah
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,
+                modifier = modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah MataKuliah"
+                )
+            }
+        }
+    ){ innerPadding -> val homeUiState by viewModel.homeUIState.collectAsState()
+
+        if (homeUiState == null) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(text = "Loading state is null")
+            }
+        } else {
+            BodyHomeMKView(
+                homeUiStateMK = homeUiState,
+                onClick = onDetailClick,
+                modifier = modifier.padding(innerPadding)
+            )
+        }
+//        val homeUiState by viewModel.homeUIState.collectAsState()
+//
+//        BodyHomeMKView(
+//            homeUiStateMK = homeUiState,
+//            onClick = {
+//                onDetailClick(it)
+//            },
+//            modifier = modifier.padding(innerPadding)
+//        )
+
+    }
+}
 
 
 @Composable
 fun BodyHomeMKView(
-    homeUiState: HomeUiState,
+    homeUiStateMK: HomeUiStateMK,
     onClick: (String) -> Unit = { },
     modifier: Modifier = Modifier
 ){
     val coroutineScope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
     when{
-        homeUiState.isLoading ->{
+        homeUiStateMK.isLoading ->{
             //menampilkan indikator loading
             Box(modifier = Modifier.fillMaxSize()
                 , contentAlignment = Alignment.Center){
                 CircularProgressIndicator() }
         }
 
-        homeUiState.isError->{
+        homeUiStateMK.isError->{
             //Menampilkan Pesan Error
-            LaunchedEffect(homeUiState.errorMessage) {
-                homeUiState.errorMessage?.let { message ->
+            LaunchedEffect(homeUiStateMK.errorMessage) {
+                homeUiStateMK.errorMessage?.let { message ->
                     coroutineScope.launch{
                         snackbarHostState.showSnackbar(message) //tampilkan snackbar
                     }
@@ -64,7 +131,7 @@ fun BodyHomeMKView(
             }
         }
 
-        homeUiState.listMK.isEmpty() -> {
+        homeUiStateMK.listMK.isEmpty() -> {
             //Menampilkan pesan jika data kosong
             Box(
                 modifier = modifier.fillMaxSize(),
@@ -80,10 +147,11 @@ fun BodyHomeMKView(
         }else ->{
 
         ListMataKuliah(
-            listMK = homeUiState.listMK,
+            listMK = homeUiStateMK.listMK,
             onClick = {
-                onClick
-            }
+                onClick(it)
+            },
+            modifier = modifier
         )
     } }
 
