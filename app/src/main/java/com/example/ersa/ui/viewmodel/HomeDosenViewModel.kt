82d106ef586b766ca500fbbdcdf1c30a1feb04ec
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 10:08:14 +0700

    Mengisi HomeDosenViewModel.kt

--- app/src/main/java/com/example/ersa/ui/viewmodel/HomeDosenViewModel.kt
+++ app/src/main/java/com/example/ersa/ui/viewmodel/HomeDosenViewModel.kt
@@ -1,2 +1,54 @@
 package com.example.ersa.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ersa.data.entity.Dosen
+import com.example.ersa.repository.RepositoryDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+
+class HomeDosenViewModel (
+    private val repositoryDosen: RepositoryDosen
+): ViewModel(){
+
+    val homeUIState: StateFlow<HomeUiState> = repositoryDosen.getAllDosen()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
+
+data class HomeUiState(
+    val listMhs: List<Dosen> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
