695730b6c9ab81fef0ebcc908588b98a39401fcc
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 09:54:50 +0700

    Create Home HomeSelectorView.kt

--- app/src/main/java/com/example/ersa/ui/view/HomeSelectorView.kt
+++ app/src/main/java/com/example/ersa/ui/view/HomeSelectorView.kt
@@ -1,7 +1,9 @@
 package com.example.ersa.ui.view
 
+import android.graphics.Paint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -11,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ersa.ui.customwidget.CustomTopAppBar
 
 
 @Preview(showBackground = false)
@@ -20,36 +24,19 @@ fun HomeSelectorView(
     //onMahasiswaClick:()-> Unit={},
     modifier: Modifier = Modifier
 ){
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        Arrangement.Center
 
-
-    ) {
-        Button(
-            onClick = {},
-        ) {
-            Text(text = "DOSEN")
-        }
-        Button(
-            onClick = {},
-        ) {
-            Text(text = "Mahasiswa")
-        }
+    Scaffold(
+        topBar = { CustomTopAppBar(
+            judul = "KRS APP",
+            showBackButton = false,
+            onBack = {},
+            modifier = modifier
+        ) }
+    ){innerPadding ->
+        BodyHomeSelectorView(
+            modifier = modifier.padding(innerPadding)
+        )
     }
-//    Scaffold(
-//        topBar = { CustomTopAppBar(
-//            judul = "KRS APP",
-//            showBackButton = false,
-//            onBack = {},
-//            modifier = modifier
-//        ) }
-//    ){//innerPadding ->
-//        //val homeUiState by viewModel.homeUIState.collectAsState()
-//        //BodyHomeSelectorView(
-//          //  Modifier = modifier.padding()
-//        )
-//    }
 }
 
 
@@ -58,6 +45,25 @@ fun BodyHomeSelectorView(
     modifier: Modifier
 ){
     Column(
-        modifier = modifier.fillMaxWidth()
-    ) {  }
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+
+    ) {
+
+        Button(
+            onClick = {},
+            Modifier.fillMaxWidth()
+
+        ) {
+            Text(text = "DOSEN")
+        }
+        Button(
+            onClick = {},
+            Modifier.fillMaxWidth()
+        ) {
+            Text(text = "MAHASISWA")
+        }
+    }
 }
