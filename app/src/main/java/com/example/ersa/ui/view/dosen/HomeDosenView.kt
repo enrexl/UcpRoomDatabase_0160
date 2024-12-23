d1077843888526fc5c9a55fa65069b58b845505e
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-23 10:12:30 +0700

    Membuat Card Dosen pada HomeDosenView.kt

--- app/src/main/java/com/example/ersa/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/ersa/ui/view/dosen/HomeDosenView.kt
@@ -1,2 +1,190 @@
 package com.example.ersa.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ersaditya.ui.view.mahasiswa.BodyHomeMhsView
+import com.ersaditya.ui.view.mahasiswa.CardMhs
+import com.ersaditya.ui.view.mahasiswa.ListMahasiswa
+import com.example.ersa.data.entity.Dosen
+import com.example.ersa.ui.customwidget.CustomTopAppBar
+import com.example.ersa.ui.viewmodel.HomeUiState
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeDosenView(
+    //viewModel: Home
+    onAddDosen:() -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = false,
+                onBack = {},
+                modifier = modifier
+            )
+
+        },
+        floatingActionButton = {                // tombol add di kanan bawah
+            FloatingActionButton(
+                onClick = onAddDosen,
+                shape = MaterialTheme.shapes.medium,
+                modifier = modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Dosen"
+                )
+            }
+        }
+    ){innerPadding ->
+        val homeUiState by viewmode
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = modifier.padding(innerPadding)
+        )
+
+    }
+}
+
+@Composable
+fun BodyHomeDosenView(
+    homeUiState: HomeUiState,
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        homeUiState.isLoading ->{
+            //menampilkan indikator loading
+            Box(modifier = Modifier.fillMaxSize()
+                , contentAlignment = Alignment.Center){
+                CircularProgressIndicator() }
+        }
+
+        homeUiState.isError->{
+            //Menampilkan Pesan Error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch{
+                        snackbarHostState.showSnackbar(message) //tampilkan snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMhs.isEmpty() -> {
+            //Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data Mahasiswa",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }else ->{
+        //menampilkan daftar Dosen
+
+    } }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardMhs(
+    dosen: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+){
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dosen.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Info, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dosen.nidn,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Info, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dosen.jenisKelamin,
+                    fontWeight = FontWeight.Bold
+                )
+
+            }
+        }
+    }
+}
