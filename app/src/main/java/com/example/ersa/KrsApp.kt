303334029e2a36872d0984e4326809f92209df03
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-24 08:49:37 +0700

    mengubah KrsApp.kt

--- app/src/main/java/com/example/ersa/KrsApp.kt
+++ app/src/main/java/com/example/ersa/KrsApp.kt
@@ -1,17 +1,13 @@
 package com.example.ersa
 
 import android.app.Application
-import android.util.Log
 import com.example.ersa.dependenciesinjection.ContainerApp
 
+class KrsApp : Application(){
+    lateinit var containerApp: ContainerApp
 
-class KrsApp: Application() {
-    lateinit var containerApp: ContainerApp // Fungsinya untuk menyimoan
-
-    override fun onCreate(){
+    override fun onCreate() {
         super.onCreate()
-        containerApp = ContainerApp(this) //membuat instance
-        Log.d("KrsApp", "containerApp initialized")
-        //instance adalah objek yang dibuat dari class
+        containerApp = ContainerApp(this)
     }
 }
